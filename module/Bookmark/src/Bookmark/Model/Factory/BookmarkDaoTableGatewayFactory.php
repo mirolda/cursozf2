2cbf54c77d64aa3484b74c78db1f8cc9ab6b6ccb
mirolda <mirentxu@gmail.com>
2015-11-04 09:55:55 +0100

    Pre Sesion 5

---
+++ module/Bookmark/src/Bookmark/Model/Factory/BookmarkDaoTableGatewayFactory.php
@@ -0,0 +1,32 @@
+<?php
+namespace Bookmark\Model\Factory;
+
+
+use Bookmark\Model\Bookmark;
+use Bookmark\Model\BookmarkDaoTableGateway;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class BookmarkDaoTableGatewayFactory implements FactoryInterface
+{
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     *
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $resultSetPrototype = new ResultSet();
+        $resultSetPrototype->setArrayObjectPrototype(new Bookmark());
+        $adapter = $serviceLocator->get('database');
+        $table = 'bookmarks';
+        $tablegateway = new TableGateway($table, $adapter, null, $resultSetPrototype);
+
+        return new BookmarkDaoTableGateway($tablegateway);
+    }
+}
